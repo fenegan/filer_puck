2e7ee0c10bcf405ed0202a9f87b19fa3b01a39d4
Killian Vermersch <killian@idequanet.com>
2018-04-05 15:32:56 +0200

    Added view function

--- Controller/FileController.php
+++ Controller/FileController.php
@@ -7,6 +7,32 @@ use Model\FileManager;
 
 class FileController extends BaseController
 {
+    public function viewAction()
+    {
+        if (isset($_GET['id']) && $id = intval($_GET['id']))
+        {
+            $fileManager = new FileManager();
+            $file = $fileManager->findById($id);
+            $path = './uploads/'.$file['id'].'_'.$file['name'];
+            $info = new \SplFileInfo($path);
+            $ext = $info->getExtension();
+            $url = '?action=download&id='.$file['id'];
+            if (in_array($ext, ['txt', 'php']))
+                $content = file_get_contents($path);
+            else
+                $content = '';
+            if ($file)
+                return $this->render('view.html.twig', [
+                    'file'    => $file,
+                    'path'    => $url,
+                    'ext'     => $ext,
+                    'content' => $content
+                ]);
+        }
+        
+        $this->redirectToRoute('home');
+    }
+    
     public function uploadAction()
     {
         if (isset($_FILES['file']) and isset($_POST['filename']))
@@ -26,10 +52,19 @@ class FileController extends BaseController
             $fileManager = new FileManager();
             $file = $fileManager->findById($id);
             $filename = './uploads/'.$file['id'].'_'.$file['name'];
+
             if (file_exists($filename)) {
-                header('Content-Description: File Transfer');
-                header('Content-Type: application/octet-stream');
-                header('Content-Disposition: attachment; filename="'.basename($file).'"');
+                if (isset($_GET['iframe']))
+                {
+                    $type = mime_content_type($filename);
+                    header('Content-Type: '.$type);
+                }
+                else
+                {
+                    header('Content-Description: File Transfer');
+                    header('Content-Type: application/octet-stream');
+                    header('Content-Disposition: attachment; filename="'.basename($file['name']).'"');
+                }
                 header('Expires: 0');
                 header('Cache-Control: must-revalidate');
                 header('Pragma: public');
